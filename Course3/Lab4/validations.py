b796bdf01ab84a9121682406e95b5b3806be688a
Gayatri <gayatri.varunprasad@gmail.com>
2023-12-26 21:10:49 +0000

    Closes: #1 added more comments

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -26,7 +26,7 @@ print(validate_user("blue.kale",3)) #True
 print(validate_user(".blue.kale",3)) #currently True, should be False
 print(validate_user("red_quinoa",4)) #True
 print(validate_user("_red_quinoa", 4)) #Currently True, should be False
-#comment line
+#comment lines
 
 
 
